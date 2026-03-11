a91a4d2bc8b838d190b5ac36b26f6f886b634e52
Juaco58 <navarroj58@gmail.com>
2026-03-11 17:01:00 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@ const ASSETS = [
   "/Checklist4Pro/",
   "./manifest.json",
   // Agrega aquí las rutas de tus iconos si los nombres son diferentes:
-  "/Checklist4Pro/android-chrome-192x192.png",
-  "/Checklist4Pro/android-chrome-512x512.png",
-  "/Checklist4Pro/maskable-icon-512x512.png"
+  "./android-chrome-192x192.png",
+  "./android-chrome-512x512.png",
+  "./maskable-icon-512x512.png"
 ];
 
 // Instala el Service Worker y guarda los archivos en caché
