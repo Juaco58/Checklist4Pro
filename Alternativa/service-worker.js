6c30f0e12ed9d5340866d00b54da79741941f586
Juaco58 <navarroj58@gmail.com>
2026-03-17 14:02:29 -0300

    Add files via upload

---
+++ Alternativa/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'checklist-pro-alternativa v1.0';
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  // Iconos en la carpeta superior (Root)
+  '../android-chrome-192x192.png',
+  '../android-chrome-512x512.png',
+  '../maskable-icon-512x512.png'
+];
+
+// Instala el Service Worker y guarda los archivos en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activa el Service Worker y limpia cachés antiguos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Responde con los archivos del caché cuando no hay conexión
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
